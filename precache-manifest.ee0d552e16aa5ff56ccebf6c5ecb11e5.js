15bf9d167910a421722717f53971419ccb304046
Claudinei Brito Junior <claudineibjr@hotmail.com>
2019-12-02 21:19:24 -0300

    Updates

---
+++ precache-manifest.ee0d552e16aa5ff56ccebf6c5ecb11e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac90fab23d68000dad03b41b5bce4b0",
+    "url": "/claudineibjr/SocialNetwork/index.html"
+  },
+  {
+    "revision": "caee33a12542ffa180fb",
+    "url": "/claudineibjr/SocialNetwork/static/css/main.d2f647f7.chunk.css"
+  },
+  {
+    "revision": "6a7919de77d39b113616",
+    "url": "/claudineibjr/SocialNetwork/static/js/2.c94141f4.chunk.js"
+  },
+  {
+    "revision": "caee33a12542ffa180fb",
+    "url": "/claudineibjr/SocialNetwork/static/js/main.15c5227b.chunk.js"
+  },
+  {
+    "revision": "cdddee144b5383cdcf90",
+    "url": "/claudineibjr/SocialNetwork/static/js/runtime-main.1b8a62a2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/claudineibjr/SocialNetwork/static/media/logo.25bf045c.svg"
+  }
+]);
