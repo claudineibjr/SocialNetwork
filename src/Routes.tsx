5c3e1123e1a1c9ddeaca27082955d3dcfcbbfbfd
Claudinei Brito Junior <claudineibjr@hotmail.com>
2019-12-04 11:14:30 -0300

    BUG FIX: Website always redirects to 404 error page on GitHub Pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Store';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './Pages/Login';
 import Home from './Pages/Home';
@@ -19,12 +19,14 @@ export enum PossibleRoutes{
 export default function Routes(){
     return(
         <Provider store={store} >
-            <BrowserRouter>
-                <Route path='/' exact component={Login}/>
-                <Route path={PossibleRoutes.LOGIN} component={Login}/>
-                <Route path={PossibleRoutes.HOME} component={Home}/>
-                <Route path={PossibleRoutes.PROFILE} component={Profile}/>
-                <Route path='*' component={NotFound}/>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Switch>
+                    <Route path='/' exact component={Login}/>
+                    <Route path={PossibleRoutes.LOGIN} component={Login}/>
+                    <Route path={PossibleRoutes.HOME} component={Home}/>
+                    <Route path={PossibleRoutes.PROFILE} component={Profile}/>
+                    <Route component={NotFound}/>
+                </Switch>
             </BrowserRouter>
         </Provider>
     )
